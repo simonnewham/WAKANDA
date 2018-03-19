64fdf975c3be625fe20179e2867fdb6da0ad950b
Shivaan Motilal <shivaanmotilal@gmail.com>
2018-03-19 09:27:53 +0200

    Need to update for terms and factors + expressions within itself

--- CompCode/src/Expression.java
+++ CompCode/src/Expression.java
@@ -1,7 +1,19 @@
+import java.util.ArrayList;
 
 public class Expression {
+	//Expression -> Expression plus Term
+	//		 | Expression minus Term
+	//               | Term
 	
-	public boolean parseExpr(){
-		return true;
+	ArrayList<String> tree = new ArrayList();//array of strings for printing the tree
+	
+	public Expression(){
+		
+	}
+	public boolean parseExpr(ArrayList<TokenTypes> tokens){
+		//         Term -> Term multiply Factor
+		//             | Term divide Factor
+		//             | Factor
+		if(tokens.get(0) == TokenTypes.TERM){
 	}
 }
