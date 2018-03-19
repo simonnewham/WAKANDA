993c5d6aa8342ab4ced256428c54f9c6ebe0643b
Ross van der Heyde <rossvdheyde@gmail.com>
2018-03-19 11:41:49 +0200

    Update Expression.java

--- CompCode/src/Expression.java
+++ CompCode/src/Expression.java
@@ -4,42 +4,43 @@ public class Expression {
 	//Expression -> Expression plus Term
 	//		 | Expression minus Term
 	//               | Term
-	
+
 	ArrayList<String> tree = new ArrayList();//array of strings for printing the tree
-	
-	public Expression(){
-		
+
+	public Expression() {
+
 	}
-	public boolean parseExpr(ArrayList<TokenTypes> tokens){
+	public boolean parseExpr(ArrayList<TokenTypes> tokens) {
 		//         Term -> Term multiply Factor
 		//             | Term divide Factor
 		//             | Factor
-		
+
 		//Instantiated Expr and Term
 		Expression expr = new Expression();
 		Term term = new Term();
 		Term term1 = new Term();
-		
-		if(expr.parseExp( tokens )){
-			if(tokens.get(0)==TokenTypes.PLUS){
+
+		if (expr.parseExpr( tokens )) {
+			if (tokens.get(0) == TokenTypes.PLUS) {
 				tokens.remove(0);
-				
-				if (term.parseExp( tokens )){
-					return True;
+
+				if (term.parseTerm( tokens )) {
+					return true;
 				}
 			}
-			if(tokens.get(0)==TokenTypes.MINUS){
+			if (tokens.get(0) == TokenTypes.MINUS) {
 				tokens.remove(0);
-				
-				if (term.parseExp( tokens )){
-					return True;
+
+				if (term.parseTerm( tokens )) {
+					return true;
 				}
 			}
-		}
-		else{
-			if( term1.parseTerm(tokens){
-				return True;
+		} else {
+			if (term1.parseTerm(tokens)) {
+				return true;
 			}
 		}
+
+		return false;
 	}
 }
