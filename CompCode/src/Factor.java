6280d21c0da18e82cfee1ef4db29abb93414053c
Ross van der Heyde <rossvdheyde@gmail.com>
2018-03-19 11:42:09 +0200

    Update Factor.java

--- CompCode/src/Factor.java
+++ CompCode/src/Factor.java
@@ -1,39 +1,38 @@
 import java.util.ArrayList;
 
 public class Factor {
-       //Factor -> lround Expression rround
-		   // | numLiteral
-		   // | identifier
-       // 	      | floatLiteral    
-        public Factor(){
+  //Factor -> lround Expression rround
+  // | numLiteral
+  // | identifier
+  //         | floatLiteral
+  public Factor() {
+  }
+
+
+  public boolean parseFactor(ArrayList<TokenTypes> tokens) {
+    //Check for nums identifier of float
+    if ( (tokens.get(0) == TokenTypes.NUMS_LITERAL) || (tokens.get(0) == TokenTypes.IDENTIFIER)
+         || (tokens.get(0) == TokenTypes.FLOAT_LITERAL) ) {
+      tokens.remove(0);
+      return true;
+    } else {
+      //Check for enclosed expression
+      if (tokens.get(0) == TokenTypes.L_ROUND) {
+        tokens.remove(0);
+
+        Expression expr = new Expression();
+        if (expr.parseExpr( tokens )) {
+          if (tokens.get(0) == TokenTypes.R_ROUND) {
+            tokens.remove(0);
+            return true;
+          }
         }
-        
-        
-        public boolean parseFactor(ArrayList<TokenTypes> tokens) {
-           //Check for nums identifier of float
-            if( (tokens.get(0) == TokenTypes.NUMS_LITERAL) || (tokens.get(0) == TokenTypes.IDENTIFIER) || (tokens.get(0) == TokenTypes.FLOAT_LITERAL) ){
-              tokens.remove(0);
-              return true;
-            }
-            //Check for enclosed expression
-            else {
-              if (tokens.get(0)==TokenTypes.L_ROUND){
-                tokens.remove(0);
-                
-                Expression expr = new Expression(); 
-                if (expr.parseExp( tokens )){
-                  if (tokens.get(0)== TokenTypes.R_ROUND){
-                    tokens.remove(0);
-                    return True;
-                  }
-                }
-                
-              }
-              else {
-                return False;
-              }
-              
-            }
-	}
-  
+
+      }
+
+    }
+
+    return false;
+  }
+
 }
