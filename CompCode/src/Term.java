b4a7f3ae7b5851a5f21a54998e469628dad8c423
Shivaan Motilal <shivaanmotilal@gmail.com>
2018-03-19 11:25:23 +0200

    Done

--- CompCode/src/Term.java
+++ CompCode/src/Term.java
@@ -1,4 +1,36 @@
 
 public class Term {
+    //Term -> Term multiply Factor
+		//  | Term divide Factor
+    //  | Factor
+  public Term(){
+  }
+  
+  public boolean parseTerm(ArrayList<TokenTypes> tokens) {
+    //Instantiate objects
+    Term term = new Term();
+    Factor factor= new Factor();
+    Factor factor1= new Factor();
+    
+    //Term + Factor or Term / Factor
+    if (term.parseTerm( tokens )){
+      if ( (tokens.get(0)== TokenTypes.MULTIPLY) || (tokens.get(0)== TokenTypes.DIVIDE)){
+        tokens.remove(0);
+        if (factor.parseFactor( tokens )){
+          return True;
+        }
+        
+      }
+      
+    }
+    
+    //Just a single Factor
+    else{
+      if (factor1.parseFactor( tokens )){
+        return True;
+      }
+    }
+    
+  }
 
 }
